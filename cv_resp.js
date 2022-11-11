c9276eb588be348bd2c9fa5c46696fc18bc99c60
do-jonathan4 <d.johnt45@gmail.com>
2022-11-10 16:13:46 -0800

    user can fill in blank and copy txt

--- cv_resp.js
+++ cv_resp.js
@@ -1,10 +1,9 @@
 const input = document.querySelectorAll('input')
-const [position, company, languages, comValue] = input
+const [position, company, other] = input
 
 const position_fill = document.querySelectorAll('.position')
 const company_fill = document.querySelectorAll('.company')
-const languages_fill = document.querySelectorAll('.languages')
-const comValue_fill = document.querySelectorAll('.comValue')
+const other_fill = document.querySelectorAll('.other')
 
 const sub = document.querySelector('#sub')
 const copy = document.querySelector('#copy')
@@ -16,15 +15,12 @@ sub.addEventListener('click', (e) =>  {
         return prompt()
     } else if (company.value === '') {
         return prompt()
-    } else if (languages.value === '') {
-        return prompt()
-    } else if (comValue.value === '') {
+    } else if (other.value === '') {
         return prompt()
     } else {
         position_fill.forEach(x => x.textContent = position.value)
         company_fill.forEach(x => x.textContent = company.value)
-        languages_fill.forEach(x => x.textContent = languages.value)
-        comValue_fill.forEach(x => x.textContent = comValue.value)
+        other_fill.forEach(x => x.textContent = other.value)
     }
 })
 copy.addEventListener('click', () => {
