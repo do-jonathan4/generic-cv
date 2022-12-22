22384f2c282af3db5b1a4c8ae8649e33fbd05a36
do-jonathan4 <d.johnt45@gmail.com>
2022-12-21 18:22:31 -0800

    edit p content

--- cv_dev.js
+++ cv_dev.js
@@ -1,10 +1,9 @@
 const input = document.querySelectorAll('input')
-const [position, company, languages, comValue] = input
+const [position, company, languages] = input
 
 const position_fill = document.querySelectorAll('.position')
 const company_fill = document.querySelectorAll('.company')
 const languages_fill = document.querySelectorAll('.languages')
-const comValue_fill = document.querySelectorAll('.comValue')
 
 const sub = document.querySelector('#sub')
 const copy = document.querySelector('#copy')
@@ -18,18 +17,14 @@ sub.addEventListener('click', (e) =>  {
         return prompt()
     } else if (languages.value === '') {
         return prompt()
-    } else if (comValue.value === '') {
-        return prompt()
     } else {
         position_fill.forEach(x => x.textContent = position.value)
         company_fill.forEach(x => x.textContent = company.value)
         languages_fill.forEach(x => x.textContent = languages.value)
-        comValue_fill.forEach(x => x.textContent = comValue.value)
     }
 })
 copy.addEventListener('click', () => {
     const cv = document.querySelector('#cv')
-    console.log(cv.innerText)
     navigator.clipboard.writeText(cv.innerText);
     alert('Text copied successfully')
 })
